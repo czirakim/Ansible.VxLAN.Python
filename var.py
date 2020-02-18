1dd3b7e6b1b46b5e3dfd0141fa8e1f0c3daea844
mihai cziraki <58424830+czirakim@users.noreply.github.com>
2020-02-18 09:53:16 +0200

    Add files via upload

---
+++ var.py
@@ -0,0 +1,12 @@
+#clagid/vPC,mlag , this can be a list but script needs to be changed
+clagid = 1
+
+#number of spine switches
+spinenumber = 2
+
+#id of the device
+# id=1 10.0.0.1 , id=2 10.0.0.2
+id = [ 1, 2 , 3]
+
+# vlans
+vlans = [ 100, 200 ]
